b228a238ed95403a792d75b5fe45ea3efd6350e6
barunstha007 <barun.shrestha6@gmail.com>
2024-05-28 19:16:09 +0545

    validation added in cryptocurrency model

--- src/models/cryptocurrency.js
+++ src/models/cryptocurrency.js
@@ -2,14 +2,39 @@ const mongoose = require('mongoose')
 
 const cryptocurrencySchema = new mongoose.Schema(
 	{
-		img: String,
-		code: String,
-		name: String,
-		price: Number,
-		marketCap: Number,
-		'24h': Number,
-		priceChangePercent: Number,
-		change:Number
+		img: {
+			type: String,
+			required: true,
+		},
+		code: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		name: {
+			type: String,
+			required: true,
+		},
+		price: {
+			type: Number,
+			required: true,
+		},
+		marketCap: {
+			type: Number,
+			required: true,
+		},
+		'24h': {
+			type: Number,
+			required: true,
+		},
+		priceChangePercent: {
+			type: Number,
+			required: false,
+		},
+		change: {
+			type: Number,
+			required: true,
+		},
 	},
 	{ timestamps: true }
 )
