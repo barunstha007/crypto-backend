ac313e3313dadba9534a36465424a1a6f868d985
barunstha007 <barun.shrestha6@gmail.com>
2024-05-28 19:25:51 +0545

    validation added to watch list api

--- src/controllers/watchlistController.js
+++ src/controllers/watchlistController.js
@@ -2,7 +2,22 @@ const Watchlist = require('../models/watchlist')
 
 exports.addToWatchlist = async (req, res) => {
 	const { code, min_price, max_price } = req.body
+
 	try {
+		if (!code || !min_price || !max_price) {
+			return res
+				.status(400)
+				.json({ error: 'Please provide code, min_price, and max_price' })
+		}
+
+		// Check if min_price is less than max_price
+		if (min_price >= max_price) {
+			return res
+				.status(400)
+				.json({ error: 'min_price must be less than max_price' })
+		}
+
+		// Create a new watchlist entry
 		const newEntry = new Watchlist({ code, min_price, max_price })
 		await newEntry.save()
 		res.status(201).json(newEntry)
